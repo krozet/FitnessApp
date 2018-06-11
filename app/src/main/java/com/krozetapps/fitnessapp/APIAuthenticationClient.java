b80291d76accfd3fbe08e4176fcb9b1cd813bad4
Keawa Rozet <keawa.rozet@yahoo.com>
2018-06-10 23:40:59 -0700

    Response outputs JSON object correctly, just need to handle invalid user/pass.

--- app/src/main/java/com/krozetapps/fitnessapp/APIAuthenticationClient.java
+++ app/src/main/java/com/krozetapps/fitnessapp/APIAuthenticationClient.java
@@ -1,4 +1,281 @@
 package com.krozetapps.fitnessapp;
 
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.Authenticator;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Credentials;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.Route;
+
 public class APIAuthenticationClient {
+
+    private String baseUrl;
+    private String username;
+    private String password;
+    private String urlResource;
+    private String httpMethod; // GET, POST, PUT, DELETE
+    private String urlPath;
+    private String lastResponse;
+    private String payload;
+    private HashMap<String, String> parameters;
+    private Map<String, List<String>> headerFields;
+
+    /**
+     *
+     * @param baseUrl String
+     * @param username String
+     * @param password String
+     */
+    public APIAuthenticationClient(String  baseUrl, String username, String password) {
+        //append a '/' to the end of the url if necessary
+        setBaseUrl(baseUrl);
+
+        this.username = username;
+        this.password = password;
+        this.urlResource = "";
+        this.urlPath = "";
+        this.httpMethod = "GET";
+        parameters = new HashMap<>();
+        lastResponse = "";
+        payload = "";
+        headerFields = new HashMap<>();
+        // This is important. The application may break without this line.
+        System.setProperty("jsse.enableSNIExtension", "false");
+    }
+
+    /**
+     * --&gt;http://BASE_URL.COM&lt;--/resource/path
+     * @param baseUrl the root part of the URL
+     */
+    public APIAuthenticationClient setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+        if (!baseUrl.substring(baseUrl.length() - 1).equals("/")) {
+            this.baseUrl += "/";
+        }
+        return this;
+    }
+
+    /**
+     * Set the name of the resource that is used for calling the Rest API.
+     * @param urlResource http://base_url.com/--&gt;URL_RESOURCE&lt;--/url_path
+     */
+    public APIAuthenticationClient setUrlResource(String urlResource) {
+        this.urlResource = urlResource;
+        return this;
+    }
+
+    /**
+     * Set the path  that is used for calling the Rest API.
+     * This is usually an ID number for Get single record, PUT, and DELETE functions.
+     * @param urlPath http://base_url.com/resource/--&gt;URL_PATH&lt;--
+     */
+    public final APIAuthenticationClient setUrlPath(String urlPath) {
+        this.urlPath = urlPath;
+        return this;
+    }
+
+    /**
+     * Sets the HTTP method used for the Rest API.
+     * GET, PUT, POST, or DELETE
+     */
+    public APIAuthenticationClient setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+        return this;
+    }
+
+    /**
+     * Get the output from the last call made to the Rest API.
+     * @return String
+     */
+    public String getLastResponse() {
+        return lastResponse;
+    }
+
+    /**
+     * Get a list of the headers returned by the last call to the Rest API.
+     * @return Map&lt;String, List&lt;String&gt;&gt;
+     */
+    public Map<String, List<String>> getHeaderFields() {
+        return headerFields;
+    }
+
+    /**
+     * Replace all of the existing parameters with new parameters.
+     * Typically used for PUT or POST functions.
+     * @param parameters
+     * @return this
+     */
+    public APIAuthenticationClient setParameters(HashMap<String, String> parameters) {
+        this.parameters = parameters;
+        return this;
+    }
+
+    /**
+     * Set a parameter to be used in the call to the Rest API.
+     * @param key the name of the parameter
+     * @param value the value of the parameter
+     * @return this
+     */
+    public APIAuthenticationClient setParameter(String key, String value) {
+        this.parameters.put(key, value);
+        return this;
+    }
+
+    /**
+     * Delete all parameters that are set for the Rest API call.
+     * @return this
+     */
+    public APIAuthenticationClient clearParameters() {
+        this.parameters.clear();
+        return this;
+    }
+
+    /**
+     * Remove a specified parameter
+     * @param key the name of the parameter to remove
+     */
+    public APIAuthenticationClient removeParameter(String key) {
+        this.parameters.remove(key);
+        return this;
+    }
+
+
+    /**
+     * Deletes all values used to make Rest API calls.
+     * @return this
+     */
+    public APIAuthenticationClient clearAll() {
+        parameters.clear();
+        baseUrl = "";
+        this.username = "";
+        this.password = "";
+        this.urlResource = "";
+        this.urlPath = "";
+        this.httpMethod = "";
+        lastResponse = "";
+        payload = "";
+        headerFields.clear();
+        return this;
+    }
+
+    /**
+     * Get the last response from the Rest API as a JSON Object.
+     * @return JSONObject
+     */
+    public JSONObject getLastResponseAsJsonObject() {
+        try {
+            return new JSONObject(String.valueOf(lastResponse));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get the last response from the Rest API as a JSON Array.
+     * @return JSONArray
+     */
+    public JSONArray getLastResponseAsJsonArray() {
+        try {
+            return new JSONArray(String.valueOf(lastResponse));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get the payload as a string from the existing parameters.
+     * @return String
+     */
+    private String getPayloadAsString() {
+        // Cycle through the parameters.
+        StringBuilder stringBuffer = new StringBuilder();
+        Iterator it = parameters.entrySet().iterator();
+        int count = 0;
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if (count > 0) {
+                stringBuffer.append("&");
+            }
+            stringBuffer.append(pair.getKey()).append("=").append(pair.getValue());
+
+            it.remove(); // avoids a ConcurrentModificationException
+            count++;
+        }
+        return stringBuffer.toString();
+    }
+
+
+    /**
+     * Make the call to the Rest API and return its response as a string.
+     * @return String
+     */
+    public String execute() {
+        String line = "";
+
+        try {
+            StringBuilder urlString = new StringBuilder(baseUrl + urlResource);
+
+            if (!urlPath.equals("")) {
+                urlString.append("/" + urlPath);
+            }
+
+            if (parameters.size() > 0 && httpMethod.equals("GET")) {
+                payload = getPayloadAsString();
+                urlString.append("?" + payload);
+            }
+
+
+            OkHttpClient client = new OkHttpClient();
+            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+            StringBuilder content = new StringBuilder("email=" + username + "&password=" + password);
+            RequestBody body = RequestBody.create(mediaType, "email=keawa.rozet42%40yahoo.com&password=12345");
+
+            Request request = new Request.Builder().url(urlString.toString()).post(body)
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .addHeader("Cache-Control", "no-cache")
+                    .addHeader("Postman-Token", "39701118-adc0-4818-ad61-0412d90a4e66")
+                    .build();
+
+           Response response = client.newCall(request).execute();
+           line = response.body().toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // If the outputStringBuilder is blank, the call failed.
+        if (!line.equals("")) {
+            lastResponse = line;
+        }
+
+        Log.i("ResponseBody!!!!!!!!", line);
+        return line;
+    }
 }
--- app/src/main/java/com/krozetapps/fitnessapp/LoginPage.java
+++ app/src/main/java/com/krozetapps/fitnessapp/LoginPage.java
@@ -1,13 +1,106 @@
 package com.krozetapps.fitnessapp;
 
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LoginPage extends AppCompatActivity {
+    private Button button_login_login;
+    private EditText editText_login_username;
+    private EditText editText_login_password;
+    private String username;
+    private String password;
+    private String baseUrl;
+    private OkHttpClient client;
+    private Request request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        baseUrl = "http://gopath.tk/login.php";
+
+        editText_login_username = findViewById(R.id.editText_login_username);
+        editText_login_password = findViewById(R.id.editText_login_password);
+        button_login_login = (Button) findViewById(R.id.button_login_login);
+
+        button_login_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    username = editText_login_username.getText().toString();
+                    password = editText_login_password.getText().toString();
+
+                    client = new OkHttpClient();
+
+                    MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+                    StringBuilder content = new StringBuilder("email=" + username + "&password=" + password);
+                    RequestBody body = RequestBody.create(mediaType, content.toString());
+
+                    request = new Request.Builder().url(baseUrl).post(body)
+                            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                            .build();
+//                    findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            Log.i("Fail", e.getMessage());
+//                            findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            Log.i("Success", response.body().string());
+                            // Hide the progress bar.
+//                            findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+
+                            if (response.priorResponse() == null) {
+                                goToLoginSuccessful();
+                            }
+                            // Login Failure
+                            else {
+                                Looper.prepare();
+                                Toast.makeText(LoginPage.this, "Login Failed", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                } catch (Exception ex) {
+                    Log.i("Exception", ex.getMessage());
+                }
+            }
+        });
+    }
+
+    /**
+     * Open a new activity window.
+     */
+    private void goToLoginSuccessful() {
+        Bundle bundle = new Bundle();
+        bundle.putString("username", username);
+        bundle.putString("password", password);
+        bundle.putString("baseUrl", baseUrl);
+
+        Intent intent = new Intent(this, LoginSuccessful.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
